85ea433a7d835421c2d5ef46b9ca18cee3991dd3
Rohit Pal <rohitpal210@gmail.com>
2019-08-16 21:58:36 +0530

    Audio UI improvement

---
+++ app/src/main/java/com/example/android/mychat/Conv.java
@@ -0,0 +1,30 @@
+package com.example.android.mychat;
+
+public class Conv {
+    public boolean seen;
+    public long timestamp;
+
+    public boolean isSeen() {
+        return seen;
+    }
+
+    public void setSeen(boolean seen) {
+        this.seen = seen;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Conv(boolean seen, long timestamp) {
+        this.seen = seen;
+        this.timestamp = timestamp;
+    }
+
+    public Conv() {
+    }
+}
